5ab7ccdd447fa3c2e9c9bed3b814bd09176d415f
Sweg <62927898+Shweggers@users.noreply.github.com>
2023-11-01 18:57:54 +1000

    finished base program and files

---
+++ app.py
@@ -0,0 +1,51 @@
+import urllib.request as urllib
+import json
+import time
+import glowbit
+
+
+from flask import Flask, render_template
+from math import cos, asin, sqrt, pi
+
+
+app = Flask(__name__)
+stick = glowbit.stick(pin=12)
+
+
+def getISSPosition():
+    # retrieved from:
+    # http://open-notify.org/Open-Notify-API/ISS-Location-Now/
+    req = urllib.Request("http://api.open-notify.org/iss-now.json")
+    response = urllib.urlopen(req)
+
+    obj = json.loads(response.read())
+    objPosition = obj['iss_position']  # ['latitude'] ['longitude']
+
+    return objPosition
+
+
+# retrieved from:
+# https://stackoverflow.com/a/21623206
+def distance(lat1, lon1, lat2, lon2):
+    r = 6371  # km
+    p = pi / 180
+
+    a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p) * \
+        cos(lat2*p) * (1-cos((lon2-lon1)*p))/2
+    return 2 * r * asin(sqrt(a))
+
+
+@app.route("/")
+def getCustomPosition():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    for i in range(3):
+        objPosition = getISSPosition()
+
+        time.sleep(1)
+        print("Latitude: " + str(objPosition['latitude']))
+        print("Longitude: " + str(objPosition['longitude']))
+        print("Distance: " + str(distance(0, 0,
+              float(objPosition['latitude']), float(objPosition['longitude']))))
---
+++ script.js
@@ -0,0 +1,100 @@
+var script = document.createElement("script");
+script.src =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyAWG_l7mrXrzdan9XsfG2ZE7oVILBVOR3M&libraries=places&callback=initMap&solution_channel=GMP_QB_addressselection_v1_cABC";
+script.async = true;
+
+// Attach your callback function to the `window` object
+function initMap() {
+  const CONFIGURATION = {
+    ctaTitle: "Start Tracking",
+    mapOptions: {
+      center: { lat: 37.4221, lng: -122.0841 },
+      fullscreenControl: true,
+      mapTypeControl: false,
+      streetViewControl: true,
+      zoom: 11,
+      zoomControl: true,
+      maxZoom: 22,
+      mapId: "",
+    },
+    mapsApiKey: "AIzaSyAWG_l7mrXrzdan9XsfG2ZE7oVILBVOR3M",
+    capabilities: {
+      addressAutocompleteControl: true,
+      mapDisplayControl: true,
+      ctaControl: true,
+    },
+  };
+  const componentForm = [
+    "location",
+    "locality",
+    "administrative_area_level_1",
+    "country",
+    "postal_code",
+  ];
+
+  const getFormInputElement = (component) =>
+    document.getElementById(component + "-input");
+  const map = new google.maps.Map(document.getElementById("gmp-map"), {
+    zoom: CONFIGURATION.mapOptions.zoom,
+    center: { lat: 37.4221, lng: -122.0841 },
+    mapTypeControl: false,
+    fullscreenControl: CONFIGURATION.mapOptions.fullscreenControl,
+    zoomControl: CONFIGURATION.mapOptions.zoomControl,
+    streetViewControl: CONFIGURATION.mapOptions.streetViewControl,
+  });
+  const marker = new google.maps.Marker({ map: map, draggable: false });
+  const autocompleteInput = getFormInputElement("location");
+  const autocomplete = new google.maps.places.Autocomplete(autocompleteInput, {
+    fields: ["address_components", "geometry", "name"],
+    types: ["address"],
+  });
+  autocomplete.addListener("place_changed", function () {
+    marker.setVisible(false);
+    const place = autocomplete.getPlace();
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    }
+    renderAddress(place);
+    fillInAddress(place);
+  });
+
+  function fillInAddress(place) {
+    // optional parameter
+    const addressNameFormat = {
+      street_number: "short_name",
+      route: "long_name",
+      locality: "long_name",
+      administrative_area_level_1: "short_name",
+      country: "long_name",
+      postal_code: "short_name",
+    };
+    const getAddressComp = function (type) {
+      for (const component of place.address_components) {
+        if (component.types[0] === type) {
+          return component[addressNameFormat[type]];
+        }
+      }
+      return "";
+    };
+    getFormInputElement("location").value =
+      getAddressComp("street_number") + " " + getAddressComp("route");
+    for (const component of componentForm) {
+      // Location field is handled separately above as it has different logic.
+      if (component !== "location") {
+        getFormInputElement(component).value = getAddressComp(component);
+      }
+    }
+  }
+
+  function renderAddress(place) {
+    map.setCenter(place.geometry.location);
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+  }
+}
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
